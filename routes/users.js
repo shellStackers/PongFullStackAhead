bf68369a4063b4f01e03a1664b64c009d5bedffe
SQuirreL4niner <eprojex.network@gmail.com>
2017-10-06 19:44:04 -0400

    Updated react or client directory

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([{
+    id:1,
+    username: "npmer$"
+  },
+  {
+    id:2,
+    username: "jpmer$"
+  }]);
 });
 
 module.exports = router;
