6f95fed7e509782a459e9e3d58345e1cdf9a2f12
Jeff Goyette <jgoyette.jg@gmail.com>
2017-10-08 23:00:10 -0500

    Updated with evil stuff from heroku

--- pong-api/src/main/java/com/pong/PongApiApplication.java
+++ pong-api/src/main/java/com/pong/PongApiApplication.java
@@ -9,6 +9,12 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 public class PongApiApplication {
 
 	public static void main(String[] args) {
+	    String ENV_PORT = System.getenv().get("PORT");
+	    String ENV_DYNO = System.getenv().get("DYNO");
+	    if(ENV_PORT != null && ENV_DYNO != null) {
+	        System.getProperties().put("server.port", ENV_PORT);
+	    }
+		
 		SpringApplication.run(PongApiApplication.class, args);
 	}
 }
