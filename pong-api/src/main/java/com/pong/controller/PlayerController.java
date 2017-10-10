cf33d7a749c5d4d18b7378fe3d01062f667b7719
Jeff Goyette <jgoyette.jg@gmail.com>
2017-10-09 19:37:01 -0500

    Fixed variable

--- pong-api/src/main/java/com/pong/controller/PlayerController.java
+++ pong-api/src/main/java/com/pong/controller/PlayerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,12 +47,12 @@ public class PlayerController {
 	}
 	
 	@PostMapping(value="/{playerId}")
-	public Mono<Player> recordGame(@RequestParam String playerId,@RequestBody Score score){
+	public Mono<Player> recordGame(@PathVariable String playerId,@RequestBody Score score){
 		return playerService.recordGame(playerId, score);
 	}
 	
 	@GetMapping("/all")
-	public Flux<List<Player>> findAllPlayers(){
-		return playerService.findAllPlayers();
+	public List<Player> findAllPlayers(){
+		return playerService.findAllPlayers().blockFirst();
 	}
 }
